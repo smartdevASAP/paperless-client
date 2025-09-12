b09ea9399cff353a74a34d348c4dbe5e9aecfc7f
kelvinTech <kelvinnjuguna357@gmail.com>
2025-09-12 23:14:02 +0300

    more code

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const route = require("../server/routes/user_routes.js");
+const cookieParser = require("cookie-parser");
 //adding document middleware
+app.use(cookieParser());
 app.use(express.json());
 
 //adding a test command
